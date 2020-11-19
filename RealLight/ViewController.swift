a760b448e30a25e93d3e56585175ba786e34a663
Nick Akindinov <akindinovnick@gmail.com>
2020-11-19 21:38:41 +0300

    Finish version with Icons

--- RealLight/ViewController.swift
+++ RealLight/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    var isLightOn = false
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        button.titleLabel?.text = "Включить"
     }
 
-
+    fileprivate func updateUI () {
+        isLightOn ? button.setTitle("Выключить", for: .normal) : button.setTitle("Включить", for: .normal)
+        
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        guard device.hasTorch else { return }
+        
+        do {
+            try device.lockForConfiguration()
+            if (device.torchMode == AVCaptureDevice.TorchMode.on) {
+                device.torchMode = AVCaptureDevice.TorchMode.off
+            } else {
+                try device.setTorchModeOn(level: 1.0)
+            }
+        } catch {
+            return
+        }
+        
+        device.unlockForConfiguration()
+    }
+    
+    @IBAction func buttonPressed() {
+        isLightOn.toggle()
+        updateUI()
+    }
+    
 }
 
